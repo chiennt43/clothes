0519556c1e08e10e9705c9db309fc7afc7e6bd28
TCM\thichien.nguyen <chiennguyen040396@gmail.com>
2018-06-18 09:15:01 +0700

    list item

--- fashion/src/app/body/body.component.ts
+++ fashion/src/app/body/body.component.ts
@@ -6,8 +6,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./body.component.scss']
 })
 export class BodyComponent implements OnInit {
-
-  constructor() { }
+  listItem = [];
+  menu = [];
+  constructor() { 
+    this.menu = [
+      "Shoes",
+      "T-Shirt",
+      "Dress",
+      "vallet",
+      "Hat",
+       "Shoes",
+      "T-Shirt",
+      "Dress",
+      "vallet",
+      "Hat",
+       "Shoes",
+      "T-Shirt",
+      "Dress",
+      "vallet",
+      "Hat",
+    ]
+    this.listItem = [
+       {
+        image:"item.jpg",
+        name: "T Shirt",
+        priceSale:"20,000",
+        priceOrigin:"30,000"
+      },
+      {
+        image:"item3.jpeg",
+        name: "T Shirt",
+        priceSale:"20,000",
+        priceOrigin:"30,000"
+      },
+       {
+        image:"item3.jpeg",
+        name: "T Shirt",
+        priceSale:"20,000",
+        priceOrigin:"30,000"
+      },
+       {
+        image:"item3.jpeg",
+        name: "T Shirt",
+        priceSale:"20,000",
+        priceOrigin:"30,000"
+      },
+       {
+        image:"item5.jpg",
+        name: "T Shirt",
+        priceSale:"20,000",
+        priceOrigin:"30,000"
+      },
+       {
+        image:"item3.jpeg",
+        name: "T Shirt",
+        priceSale:"20,000",
+        priceOrigin:"30,000"
+      }
+      
+    ]
+  }
 
   ngOnInit() {
   }
